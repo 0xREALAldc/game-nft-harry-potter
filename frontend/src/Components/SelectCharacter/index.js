19d82ee38146fb83ae093d605f3aceb807bbdb19
REALaldc <realaldc@REALaldcs-MacBook-Pro.local>
2022-09-03 12:23:24 -0300

    included useEffect’s to store the gameContract, also to retrieve the characters to player be able to mint his NFT, listener for the emit event to notify the user when the NFT was minted, rendered the UI to show the characters to mint, minting action on the UI

--- frontend/src/Components/SelectCharacter/index.js
+++ frontend/src/Components/SelectCharacter/index.js
@@ -1,65 +1,115 @@
 import React, { useEffect, useState } from "react";
 import "./SelectCharacter.css";
+import { ethers } from 'ethers';
+import { CONTRACT_ADDRESS, transformCharacterData } from '../../constants';
+import myEpicGame from '../../utils/MyEpicGame.json';
 
 const SelectCharacter = ({ setCharacterNFT }) => {
+  const [characters, setCharacters] = useState([]);
+  const [gameContract, setGameContract] = useState(null);
+
+  useEffect(() => {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const gameContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        myEpicGame.abi,
+        signer
+      );
+
+      setGameContract(gameContract);
+    } else {
+      console.log("Ethereum object not found!");
+    }
+  }, []);
+
+  useEffect(() => {
+    const getCharacters = async () => {
+      try {
+        console.log("Retrieving the characters for players to mint...");
+
+        const charactersTxn = await gameContract.getAllDefaultCharacters();
+        console.log("charactersTxn: ", charactersTxn);
+
+        const characters = charactersTxn.map((characterData) => 
+          transformCharacterData(characterData)
+        );
+
+        setCharacters(characters);
+      } catch (error) {
+        console.log("Something went wrong when retrieving the characters: ", error);
+      }
+    };
+
+    const onCharacterMint = async (sender, tokenId, characterIndex) => {
+      console.log(`CharacterNFTMinted - sender: ${sender}, tokenId: ${tokenId.toNumber()}, characterIndex: ${characterIndex.toNumber()}`);
+      
+      if (gameContract) {
+        const characterNFT = await gameContract.checkIfUserHasNFT();
+        console.log("CharacterNFT: ", characterNFT);
+        setCharacterNFT(transformCharacterData(characterNFT));
+
+        alert(
+          `Your NFT is minted -- check here: https://testnets.opensea.io/assets/${gameContract}/${tokenId.toNumber()}`
+        );
+      }
+    };
+
+    if (gameContract) {
+      getCharacters();
+
+      // here we listen to the event that is emitted when a NFT is minted
+      gameContract.on("CharacterNFTMinted", onCharacterMint);
+    }
+
+    // when the component is destroyed, we clear this listener
+    return () => {
+      if (gameContract) {
+        gameContract.off("CharacterNFTMinted", onCharacterMint);
+      }
+    };
+    
+  }, [gameContract]);
+
+  const renderCharacters = () => characters.map((character, index) => (
+    <div className="character-item" key={character.name}>
+      <div className="name-container">
+        <p>{character.name}</p>
+      </div>
+
+      <img src={character.imageURI} alt={character.name} />
+      <button 
+        type="button"
+        className="character-mint-button"
+        onClick={mintCharacterNFTAction(index)}
+        >{`Mint ${ character.name}`}
+      </button>
+    </div>
+  ));
+
+  const mintCharacterNFTAction = (characterId) => async () => {
+    try {
+      if (gameContract) {
+        console.log("Minting character...");
+
+        const mintTxn = await gameContract.mintCharacterNFT(characterId);
+        await mintTxn.wait();
+        console.log("mintTxn: ", mintTxn);
+      }
+    } catch (error) {
+      console.warn("MintCharacterActionError: ", error);
+    }
+  };
+
   return (
     <div className="select-character-container">
-      <h2>Mint your character</h2>
-      <div className="select-character-container character-grid">
-        <div className="character-grid character-item">
-          {/* <img 
-             src="" 
-             alt="Sirius Black" 
-             className="character-item"
-           /> */}
-
-          {/* <p className="character-item name-container">
-            Sirius Black
-          </p>
-
-          <button
-            className="character-mint-button">
-              Mint
-            </button> */}
-
-        </div>
-
-        <div className="character-grid character-item">
-          <img 
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrPf1xAS34CuVJKgukqWzrQwFoeK8MLKnyKA&usqp=CAU" 
-            alt="Sirius Black" 
-            className="character-item"
-          />
-
-          <p className="character-item name-container">
-            Sirius Black
-          </p>
-
-          <button
-            className="character-mint-button">
-              Mint
-            </button>
-
-        </div>
-
-        <div className="character-grid character-item">
-          {/* <img 
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrPf1xAS34CuVJKgukqWzrQwFoeK8MLKnyKA&usqp=CAU" 
-            alt="Sirius Black" 
-            className="character-item"
-          />
-
-          <p className="character-item name-container">
-            Sirius Black
-          </p>
-
-          <button
-            className="character-mint-button">
-              Mint
-            </button> */}
-
-        </div>
-      </div>
+      <h2>Mint your hero</h2>
+      {characters.length > 0 && (
+        <div className="character-grid">{renderCharacters()}</div>
+      )}
     </div>
   );
 };
